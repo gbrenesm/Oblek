2b846786eea078b3ae848f190f2187d68dc59ab1
Guadalupe Brenes <brenesmesa@gmail.com>
2021-01-11 15:20:03 -0600

    Card styles done

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,26 +1,36 @@
 import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons'
+import { faMinus } from '@fortawesome/free-solid-svg-icons'
 
 
 function Card({character}) {
   const [open, setOpen] = useState(false)
   return (
     <div className='card' onClick={() => setOpen(!open)}>
-      {!open &&
+      {open &&
       <div className="basic">
         <img src={character.image} alt="Character"/>
-        <p>{character.name} {open? <span><FontAwesomeIcon icon={faMinus}/></span> : <span><FontAwesomeIcon icon={faPlus} /></span>}</p>
+        <p>{character.name} <span><FontAwesomeIcon icon={faMinus}/></span></p>
       </div>}
-      {open && 
+      {!open && 
       <div className="more-info" >
         <div>
-          <div>
-            <p>Name: <u>{character.name}</u> </p>
-            <p>Species: {character.species}</p>
-          </div>
-          <img src={character.image} alt="Character"/>
+          <h2>Personal card</h2>
+          <p>No. {character.id}/671</p>
         </div>
+        <div>
+          <div>
+            <p>Name: <u>{character.name}</u></p>
+            <p>Species: <u>{character.species}</u></p>
+            <p>Gender: <u>{character.gender}</u></p>
+          </div>
+          <div>
+            <img src={character.image} alt="Character"/>
+          </div>
+        </div>
+        <p>Origin: <u>{character.origin.name}</u></p>
+        <p>Last know location: <u>{character.location.name}</u></p>
+        {character.status === 'Dead' && <img src="Dead_stamp.png"/>}
       </div>}
     </div>
   )
