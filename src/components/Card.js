4e5d98a6a7d2792cdbe8ca68f53cd948c729ee9a
Guadalupe Brenes <brenesmesa@gmail.com>
2021-01-11 13:36:58 -0600

    Addign fotns for card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,27 @@
 import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons'
+import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons'
 
 
 function Card({character}) {
   const [open, setOpen] = useState(false)
   return (
     <div className='card' onClick={() => setOpen(!open)}>
-      <img src={character.image} alt="Character"/>
-      <p>{character.name} {open? <span><FontAwesomeIcon icon={faChevronUp}/></span> : <span><FontAwesomeIcon icon={faChevronDown} /></span>}</p>
+      {!open &&
+      <div className="basic">
+        <img src={character.image} alt="Character"/>
+        <p>{character.name} {open? <span><FontAwesomeIcon icon={faMinus}/></span> : <span><FontAwesomeIcon icon={faPlus} /></span>}</p>
+      </div>}
+      {open && 
+      <div className="more-info" >
+        <div>
+          <div>
+            <p>Name: <u>{character.name}</u> </p>
+            <p>Species: {character.species}</p>
+          </div>
+          <img src={character.image} alt="Character"/>
+        </div>
+      </div>}
     </div>
   )
 };
