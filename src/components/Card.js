9fab0d010486a3f1828a8351fa31032959d7e782
Guadalupe Brenes <brenesmesa@gmail.com>
2021-01-11 12:31:52 -0600

    Card basic info and style

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons'
+
 
 function Card({character}) {
+  const [open, setOpen] = useState(false)
   return (
-    <div>
-      <p>{character.name}</p>
+    <div className='card' onClick={() => setOpen(!open)}>
+      <img src={character.image} alt="Character"/>
+      <p>{character.name} {open? <span><FontAwesomeIcon icon={faChevronUp}/></span> : <span><FontAwesomeIcon icon={faChevronDown} /></span>}</p>
     </div>
   )
-}
+};
 
-export default Card
+export default Card;
